e5c66dd8b6cf17765ce16542a7a01397c24a22be
Megax <megax@yeahunter.hu>
2013-03-16 17:58:54 +0100

    * Felesleges drop rész törölve.

--- Sql/Drop/MySql.sql
+++ Sql/Drop/MySql.sql
@@ -6,8 +6,5 @@ DROP TABLE IF EXISTS
 `localized_console_command`,
 `localized_console_command_help`,
 `localized_console_warning`,
-`localized_command`,
-`localized_command_help`,
-`localized_warning`,
 `schumix`,
 `uptime`;
